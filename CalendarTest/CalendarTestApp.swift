a3a151a21526b12a6e293f858499bdf69ef03e8c
Elvish <elvish.zy@gmail.com>
2020-12-14 20:05:26 +0800

    Initial Commit

---
+++ CalendarTest/CalendarTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarTestApp.swift
+//  CalendarTest
+//
+//  Created by Elvish on 2020/12/14.
+//
+
+import SwiftUI
+
+@main
+struct CalendarTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
